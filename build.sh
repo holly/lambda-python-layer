f832d1914dc1f0119005157f4549acc942389ba0
holly <emperor.kurt@gmail.com>
2022-12-29 20:31:12 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,5 +15,4 @@ if [ -d $SRC ]; then
 fi
 mkdir $SRC
 
-#docker run --hostname $APP --name $APP --rm --cpu-period=1000000 --cpu-quota=800000 --memory=128m --mount type=bind,src=/data/lambda,dst=/data -it $TAG:latest
 docker run --hostname $APP --name $APP --rm --cpu-period=1000000 --cpu-quota=800000 --memory=128m --mount type=bind,src=$SRC,dst=$DST -it $TAG:latest
