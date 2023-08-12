a23868e14e8489f8b7b4887a62cc1c7bdc6361ee
holly <webmaster@holly9.uk>
2023-08-12 21:28:15 +0900

    add python 3.11

--- deploy.sh
+++ deploy.sh
@@ -18,4 +18,4 @@ fi
 aws lambda publish-layer-version \
 --layer-name $LAMBDA_LAYER_NAME \
 --zip-file fileb://$LAMBDA_ZIP_ARCHIVE \
---compatible-runtimes python3.8 python3.9 python3.10
+--compatible-runtimes python3.8 python3.9 python3.10 python3.11
